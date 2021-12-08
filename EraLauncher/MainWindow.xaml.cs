cb5c151f263cb912bd8277d9fcdf6618337edf96
Mateusz (Matthew) <70480433+xmatid@users.noreply.github.com>
2021-12-08 16:58:18 +0100

    Window is moving + Close Button

--- EraLauncher/MainWindow.xaml.cs
+++ EraLauncher/MainWindow.xaml.cs
@@ -1,37 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace EraLauncher
 {
-    /// <summary>
-    /// Logika interakcji dla klasy MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
-      //  public List<FV> Versions
+        //  public List<FV> Versions
         //{
-       //     get; set;
+        //     get; set;
         //}
+
 
         public MainWindow()
         {
             InitializeComponent();
 
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
 
         }
+
+        private void GridBG_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void UpperPanel_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void UpperPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void UpperPanel_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void GridBG_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void GridBG_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
-
-
 }
