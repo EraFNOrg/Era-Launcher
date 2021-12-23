e7d48de4ba652df3efef50c25698ef43549d5d10
Daniele <emiliogiorgini40@gmail.com>
2021-12-23 15:00:41 -0800

    Injecting and launching (build with x64)

--- EraLauncher/Home.xaml.cs
+++ EraLauncher/Home.xaml.cs
@@ -19,7 +19,9 @@ using IniParser;
 using IniParser.Model;
 using System.Net;
 using System.IO;
-
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Threading;
 
 namespace EraLauncher
 {
@@ -186,12 +188,141 @@ namespace EraLauncher
             }
         }
 
+        [DllImport("kernel32")]
+        public static extern IntPtr CreateRemoteThread(
+  IntPtr hProcess,
+  IntPtr lpThreadAttributes,
+  uint dwStackSize,
+  UIntPtr lpStartAddress,
+  IntPtr lpParameter,
+  uint dwCreationFlags,
+  out IntPtr lpThreadId
+);
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenProcess(
+            UInt32 dwDesiredAccess,
+            Int32 bInheritHandle,
+            Int32 dwProcessId
+            );
+
+        [DllImport("kernel32.dll")]
+        public static extern Int32 CloseHandle(
+        IntPtr hObject
+        );
+
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        static extern bool VirtualFreeEx(
+            IntPtr hProcess,
+            IntPtr lpAddress,
+            UIntPtr dwSize,
+            uint dwFreeType
+            );
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Ansi, ExactSpelling = true)]
+        public static extern UIntPtr GetProcAddress(
+            IntPtr hModule,
+            string procName
+            );
+
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        static extern IntPtr VirtualAllocEx(
+            IntPtr hProcess,
+            IntPtr lpAddress,
+            uint dwSize,
+            uint flAllocationType,
+            uint flProtect
+            );
+
+        [DllImport("kernel32.dll")]
+        static extern int WriteProcessMemory(
+            IntPtr hProcess,
+            IntPtr lpBaseAddress,
+            byte[] lpBuffer,
+            uint nSize,
+            int lpNumberOfBytesWritten
+        );
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr GetModuleHandle(
+            string lpModuleName
+            );
+
+        [DllImport("kernel32", SetLastError = true, ExactSpelling = true)]
+        internal static extern Int32 WaitForSingleObject(
+            IntPtr handle,
+            Int32 milliseconds
+            );
+
+        public static bool Inject(int P, string DllPath)
+        {
+            IntPtr hndPrc = OpenProcess(0x1F0FFF, 1, P);
+
+            if (hndPrc == IntPtr.Zero) return false;
+
+            IntPtr lpAddress = VirtualAllocEx(hndPrc, (IntPtr)null, (uint)DllPath.Length, 0x1000, 0x40);
+
+            if (lpAddress == IntPtr.Zero) return false;
+
+            byte[] bytes = Encoding.ASCII.GetBytes(DllPath);
+
+            if (WriteProcessMemory(hndPrc, lpAddress, bytes, (uint)bytes.Length, 0) == 0) return false;
+
+            UIntPtr Injector = (UIntPtr)GetProcAddress(GetModuleHandle("kernel32.dll"), "LoadLibraryA");
+
+            IntPtr bytesout;
+            IntPtr hThread = (IntPtr)CreateRemoteThread(hndPrc, (IntPtr)null, 0, Injector, lpAddress, 0, out bytesout);
+
+            CloseHandle(hndPrc);
+
+            return true;
+        }
+
+        public static int PreInject(int ProcessID, string DllPath)
+        {
+            if (!File.Exists(DllPath)) return 0;
+
+            if (ProcessID == 0) return 1;
+
+            if (!Inject(ProcessID, DllPath)) return 2;
+
+            return 3;
+        }
+
+        public void StartFortnite()
+        {
+            var Fortnite = new Process
+            {
+                StartInfo =
+                    {
+                        FileName = System.IO.Path.Combine(CurrentVersion.path, @"FortniteGame\Binaries\Win64\FortniteClient-Win64-Shipping.exe"),
+                        Arguments = "-noeac -nobe -fltoken=none -NoCodeGuards -epicapp=Fortnite -epicenv=Prod -epicportal -epiclocale=en-us -skippatchcheck -NOSSLPINNING -AUTH_LOGIN=ERA@ERA.ERA -AUTH_TYPE=epic -AUTH_PASSWORD=ERA",
+                        UseShellExecute = false
+                    }
+            };
+
+            Fortnite.Start();
+            Fortnite.WaitForInputIdle();
+
+            string strExeFilePath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            string strWorkPath = System.IO.Path.GetDirectoryName(strExeFilePath);
+
+            PreInject(Fortnite.Id, System.IO.Path.Combine(strWorkPath + @"\EraV2.dll"));
+        }
+
         private void HandleGameLaunch(object sender, RoutedEventArgs e)
         {
             // Here, danii
             if (CurrentVersion != null)
             {
-
+                if (Process.GetProcessesByName("FortniteClient-Win64-Shipping").Length == 0)
+                {
+                    new Thread(new ThreadStart(StartFortnite)).Start();
+                }
+                else
+                {
+                    MessageBox.Show("Fortnite is already running.", "ERA Launcher", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
             }
             else
             {
